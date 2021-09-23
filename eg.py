dbb1c74697282cff75f8fa5a234ec545dc50bb8f
Tim Menzies <timm@ieee.org>
2021-09-22 23:31:27 -0400

    save

--- eg.py
+++ eg.py
@@ -1,5 +1,51 @@
 #!/usr/bin/env python3.9
-from evenless import *
+from reason import *
+import argparse,traceback
+
+class Eg:
+  _all, _crash = {}, -1
+
+  def one(f): 
+    "`@Eg.one` : registers a function."
+    if f.__name__[0] != "_": Eg._all[f.__name__] = f
+    return f
+
+  def _cli(usage,dict):
+    a = argparse
+    p = a.ArgumentParser(prog=usage, description=__doc__,
+                         formatter_class=a.RawTextHelpFormatter)
+    used, add  = {}, p.add_argument
+    for k, (ako, x, h) in sorted(dict.items()):
+      k0 = k[0]
+      k0 = k0.upper() if k0 in used else k0
+      c = used[k0] = k0  # (3)
+      if x == False: add("-"+c, dest=k, default=False, help=h, action="store_true")
+      else: add("-"+c, dest=k, default=x,help=h+" [" + str(x) + "]",type=type(x), metavar=k)
+    return p.parse_args().__dict__
+ 
+  def _run(my, f):
+    random.seed(my.seed)
+    try: f(my); print(f"\x1b[1;32m✔ {f.__name__}\x1b[0m") # print green
+    except Exception as err:
+      Eg._crash += 1
+      print(f"\x1b[1;31m✘  {f.__name__}\x1b[0m") # print red
+      if my.loud: print(traceback.format_exc())
+  
+  def run():
+    """Eg.run()` : update config with any command-line flags then
+    run any todos  (resetting the random number seed  beforehand)."""
+    my = o(**Eg._cli("python3.9 eg.py [OPTIONS]",CONFIG))
+    if my.Todo: 
+      return [print(f"{name:>15} : {f.__doc__ or ''}") 
+              for name,f in Eg._all.items()]
+    if my.todo == "all":
+      for _,f in Eg._all.items(): Eg._run(kopy(my),f)
+      print("Errors:", Eg._crash)
+      sys.exit(Eg._crash)
+    else:
+      if my.todo: Eg._run(my, Eg._all[my.todo])
+
+#--------------------------------------------------
 eg = Eg.one
 
 @eg  
@@ -14,17 +60,24 @@ def show(my):
   print(my)
 
 @eg
-def num(my):
-  "report sd of some numbers"
-  n=Num(init=[9,2,5,4,12,7,8,11,9,3,7,
-              4,12,5,4,10,9,6,9,4])
-  assert int(100*n.sd) == 306
-  assert n.mu  == 7
+def csvs(my):
+  for row  in csv("data/auto93.csv"):
+    print(row)
 
 @eg
-def sym(my):
-  "report entropy of some symbols"
-  s=Sym(init="yyyyyyyyynnnnn")
-  assert int(100*ent(s.has)) == 94 
+def data(my):
+  d= slurp(my,my.file)
+  #for row in d.rows: print(row)
+  print(d.lo)
+  print(d.hi)
+
+@eg
+def dists(my):
+  data= slurp(my,my.file)
+  for row1 in  random.sample(data.rows,10):
+    d,row2 = far(data,row1)
+    print("")
+    print(row1.cells)
+    print(row2.cells,int(100*d))
 
 Eg.run()
---
+++ reason.py
@@ -0,0 +1,106 @@
+import re,math,random,functools
+
+CONFIG = dict(    
+  far  = (float,0.9, "how far to reach for 'distant' examples"),
+  file = (str, "data/auto93.csv", "input data file"),
+  loud = (bool, False,"loud mode: print stacktrace on error"),    
+  p    = (int,  2,    "co-efficient on distance equation"),    
+  samples=(int,128,    "number of samples used when finding far items"),
+  seed = (int,  10014,  "random number seed"),      
+  todo = (str,  "",   "todo: function (to be run at start-up)"),     
+  Todo = (str, False, "list available items for -t"))     
+
+def Data(my): return o(my=my,lo={}, hi={}, x={}, y={},rows=[], w={}, head=[])
+def Row(cells=[]): return o(cells=cells, used=False)
+
+def add(i,a): 
+  (_update if i.lo else _create)(i,a)
+
+def _create(i,a):
+  skip   = lambda s: "?" in s
+  less   = lambda s: "-" in s
+  num    = lambda s: s[0].isupper()
+  goal   = lambda s: "!" in s or "+" in s or "-" in s
+  i.head = a
+  for c,s in enumerate(a):
+    if skip(s): continue
+    i.w[c] = -1 if less(s) else 1
+    (i.y if goal(s) else i.x)[c]=c
+    if num(s): i.lo[c], i.hi[c] = 1E21,-1E21
+
+def _update(i,a):
+  for c in i.lo: 
+    z = a[c]
+    if z=="?": continue
+    a[c]    = z = float(z)
+    i.lo[c] = min(z, i.lo[c])
+    i.hi[c] = max(z, i.hi[c])
+  i.rows += [Row(cells=a)]
+
+def dist(i,a,b):
+  "Aha's distance metric."
+  d, n = 0, 1E-32
+  for c,x in pairs(a.cells, i.x):
+    d += _dist1(i, c, x, b.cells[c])**i.my.p
+    n += 1
+  return (d/n)**(1/i.my.p)
+
+def _dist1(i,c,x,y):
+  if   x==y=="?": return 1
+  elif c in i.lo:
+    lo, hi = i.lo[c], i.hi[c]
+    if   x=="?" : x,y = _xy(x,lo,hi)
+    elif y=="?" : y,x = _xy(y,lo,hi)
+    else        : x,y = _norm(x,lo,hi), _norm(y,lo,hi)
+    return abs(x-y)
+  else:
+    return 0 if x==y else 1
+
+def _norm(x,lo,hi): 
+  return 0 if abs(lo==hi)<1E-32 else (x-lo)/(hi-lo)
+
+def _xy(x,lo,hi): 
+  x = _norm(x,lo,hi)
+  return x, (0 if x>.5 else 1)
+
+def far(i,row1,rows=None):
+  """for `my.samples` selections from `rows`, sort the distance
+  and return the item `my.far`-th to maximum distance"""
+  rows = rows or i.rows
+  rows = random.sample(rows, min(len(rows), i.my.samples))
+  rows = sorted([(dist(i,row1,row2),row2) for row2 in rows], key=first)
+  return rows[int(i.my.far*len(rows))]
+
+def slurp(my,f):
+  i = Data(my)
+  for a in csv(f): add(i,a)
+  return i
+
+#----------------------------------------------------------
+class o:
+  "return a class can print itself (hiding 'private' keys)"
+  def __init__(i, **d)  : i.__dict__.update(d)
+  def __repr__(i) : return "{"+ ', '.join([
+    f":{k} {v}" for k, v in i.__dict__.items() if  k[0] != "_"])+"}"
+
+def csv(file, sep=",", dull=r'([\n\t\r ]|#.*)'):
+  "read csv files"
+  with open(file) as fp:
+    for s in fp:
+      s=re.sub(dull,"",s)
+      if s: 
+        yield s.split(sep)
+
+def first(a):
+  "return first item"
+  return a[0]
+
+def pairs(z, cols={}):
+  """iterate across lists or dictionaries, returning items
+  and their index (or key). if `cols` is supplied, restrict
+  the iteration to `cols`."""
+  if cols:
+    for c in cols: yield c, z[c]
+  else:
+    for c,x in (z.items() if type(z)==dict else enumerate(z)):
+      yield c,x
