38c4906caf39ef7fe922e84bd154d52932edbcc0
Tim Menzies <timm@ieee.org>
2021-09-27 01:39:36 -0400

    save

--- src/about.lua
+++ src/about.lua
@@ -1,11 +1,11 @@
 -- vim: filetype=lua ts=3 sw=3 sts=3 et :
 
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
-local ask,defaults,obj,map,dump,fmt,col
+local ask,config,obj,map,dump,pump,fmt,col,banner
 local rand,randi,srand,Seed,per,add
-local inf,cat = 1E32, table.concat
 local Eg,Num,Sample,Skip,Sym = {},{},{},{},{}
-local banner=[[
+local inf,cat = 1E32, table.concat
+banner=[[
   _________
  /_  ___   \
 /@ \/@  \   \      reason (v1.0)
@@ -17,7 +17,7 @@ local banner=[[
    \______/\\\\\   (in less that 250 LOC)
     _||_||_ ]]
 
-function defaults()  return  {
+function config()  return  {
    far=  ask("-f", .9,      'where to look for far things'),
    help= ask("-h", false,   'show  help');
    loud= ask("-l", false,   'set verbose'),
@@ -30,52 +30,81 @@ function defaults()  return  {
 -- ----------------------------------------------
 -- ## Classes
 
--- ### Create
+-- **add(i:column, x:any)**    
+-- Add things  to columns. If `x` is a table, add all its items. 
+-- Else if it is not `?`, increment  the  counter and call `:add(x)`.
+function add(i,x) 
+   if   type(x)=="table" 
+   then for _,y in pairs(x) do  add(i,y) end
+   else if x ~="?" then i.n = i.n + 1; i:add1(x) end end
+   return i end
 
--- **Num:new(?at:int, ?txt:str, ?max:int=256): Num**   
--- Summarize numerics.
-function  Num:new(at,txt,max) -- for numeric columns
-   txt = txt or ""
-   return  obj(self,"Num",
-     {n=0, at=at or 0, txt=txt, w=(txt:sub(#txt,#txt)=="-" and -1 or 1),
-      lo=inf, hi=-inf, _all={}, old=false, most=n or 256}) end
+-- ### Skip
 
 -- **Skip:new(?at:int, ?txt:str, ?keep:bool=true)**   
 -- Column of values  to ignore
 function Skip:new(keep)
    return obj(self,"Sample", {n=0, at or 0, txt=txt or ""}) end
 
--- **Sample:new(?keep:bool=true)**   
--- Store rows, summarizing the columns.
-function Sample:new(keep)
-   return obj(self,"Sample",
-      {n=0, rows={}, cols={}, x={}, y={}, keep=keep or true}) end
+-- ### Num
+
+-- **Num:new(?at:int, ?txt:str, ?max:int=256): Num**   
+-- Summarize numerics.
+function  Num:new(at,txt,max,    s) -- for numeric columns
+   txt = txt or ""
+   s   = txt:sub(#txt,#txt)
+   return  obj(self,"Num",
+     {n=0, at=at or 0, txt=txt, w=(s=="-" and -1 or  (s=="+" and 1 or 0)),
+      lo=inf, hi=-inf, _all={}, old=false, most=n or 256}) end
+-- **Num:add1(x:any)**
+function Num:add1(x)
+   if x > self.hi then self.hi=x end
+   if x < self.lo then self.lo=x end
+   if #self._all < self.most then 
+      self.old=true
+      self._all[ 1+#self._all ] =x 
+   elseif rand() < self.most/self.n then 
+      self.old=true
+      self._all[ 1+(rand()*#self._all)//1  ] = x end end
+-- **Num:all(): table**       
+-- Return `_all`, sorted.
+function Num:all()
+   if self.old then table.sort(self._all) end
+   self.old = false
+   return self._all end
+-- **Num:mid(): num**       
+function Num:mid() return per(self:all(), .5) end
+-- **Num:var(): number**
+function Num:var(  a) 
+    a=self:all(); return (per(a,.9) - per(a,.1))/2.56 end
+
+
+-- ### Sym
 
 -- **Sym:new(?at:int, ?txt:str): Sym**   
 -- Summarize symbols.
 function Sym:new(at, name) -- for symbolic columns
    return obj(self,"Sym", {n=0,at=at,txt=txt,has={},mode="",most=0}) end
+-- **Sym:add(x:any)**
+function Sym:add1(x)
+  self.has[x] = 1 + self.has[x] 
+  if self.has[x] > self.most then self.mode,self.most =x,self.has[x] end end
+-- **Sym:mid(): any**   
+function Sym:mid() return self.mode end
+-- **Sym:var(): number**
+function Sym:var(    e)
+   e=0; for _,v in pairs(self.has) do e=e-v/self.n * math.log(v/self.n,2) end
+   return e end
 
--- ### update
+-- ### Sample
 
--- **add(i:column, x:any)**
--- If `x` is a table, add all its items. Else if it is not `?`, 
--- increment  the  counter and call `:add(x)`.
-function add(self,x) 
-   if type(x)=="table" then for _,y in pairs(self) do add(self,y) end 
-   elseif x ~="?"      then self.n = self.n + 1; self:add(x)      end 
-   return self end
-
--- **Num:add(x:any)**
-function Num:add(x,  pos)
-   if x > self.hi then self.hi=x end
-   if x < self.lo then self.lo=x end
-   if #self._all < self.most then pos= 1+#self._all end
-   if rand() < self.most/self.n then pos= 1+(rand()*#self._all)//1 end
-   if pos then self.old=true; self._all[pos]=x end end
-
+-- **Sample:new(?keep:bool=true)**   
+-- Store rows, summarizing the columns.
+function Sample:new(keep)
+   return obj(self,"Sample",
+      {n=0, rows={}, cols={}, x={}, y={}, keep=keep or true}) end
 -- **Sample:add(x:table)**
-function Sample:add(t,     klass,num) 
+function Sample:add1(t,     klass,num) 
    function klass(s) return s:find("+") or s:find("-") or s:find("!") end
    function num(s)   return s:sub(1,1):match("[A-Z]") end
    if   #self.cols > 0
@@ -83,37 +112,11 @@ function Sample:add(t,     klass,num)
         if   self.keep 
         then self.rows[1+#self.rows] = t end 
    else for c,s in pairs(t) do
-           new = s:find("?") and Skip or (num(s) and Num or Sym)(c,s)
+           new = (s:find("?") and Skip or (num(s) and Num or Sym))(c,s)
            self.cols[ 1 + #self.cols ] = new
            if not s:find("?") then
               what = self[klass(s) and "y" or "x"]
               what[ 1+#what ] = new end end end end
-
--- **Sym:add(x:any)**
-function Sym:add(x)
-  self.has[x] = 1 + self.has[x] 
-  if self.has[x] > self.most then self.mode,self.most =x,self.has[x] end end
-
--- ### Query
-
--- **Num:all(): table**       
--- Return `_all`, sorted.
-function Num:all()
-   if self.old then table.sort(self._all) end
-   self.old = false
-   return self._all end
-
--- **Num:mid(): num**       
-function Num:mid() return per(self:all(), .5) end
--- **Sym:mid(): any**   
-function Sym:mid() return self.mode end
-
--- **Sym:var(): number**
-function Sym:var(    e)
-   e=0; for _,v in pairs(self.has) do e=e-v/self.n * math.log(v/self.n,e) end
-   return e end
--- **Num:var(): number**
-function Num:var() a=self:all(); return (per(a,.9) - per(a,.1))/2.56 end
 
 -- ----------------------------------------------
 -- ## Misc
@@ -138,18 +141,18 @@ local function csv(file,      split,stream,tmp)
 -- ### Maths
 
 -- **per(t:table, ?p:float=0.5): number**   
-function per(t,p)  return t[math.min(#t, 1+(p or .5)*#t)] end
+function per(t,p,  j)   
+   return t[ math.min(#t, 1+(p or .5)*#t) //1] end
 
 -- `srand` resets the random number seed;   
 -- `randi,rand` generate random integers or floats
 local Seed = 10014
 function srand(s) Seed= s or 10013 end
 function randi(lo,hi) return math.floor(0.5 + rand(lo,hi)) end
-function rand(lo,hi,     mult,mod)
-   lo,hi = lo or 0, hi or 1
-   mult, mod = 16807, 2147483647
-   Seed = (mult * Seed) % mod 
-   return lo + (hi-lo) * Seed / mod end 
+function rand(lo,hi)
+   lo, hi = lo or 0, hi or 1
+   Seed = (16807 * Seed) % 2147483647 
+   return lo + (hi-lo) * Seed / 2147483647 end 
 
 -- ### Meta
 
@@ -180,6 +183,10 @@ function ask(flag,old,_)
 
 -- ### Strings
 
+-- **p(t:table): string**    
+-- Print table as string.
+function pump(t) print(dump(t)) end
+ 
 -- **dump(t:table): string**    
 -- Convert table to string.
 function dump(t) 
@@ -199,10 +206,13 @@ function fmt( ...) return string.format(...) end
 local Eg={}
 Eg["hello"]=function() print(banner) end
 Eg["dump"]= function() print(dump{a="aaa",b="bbb"}) end
-Eg["num"]=  function(    n)
-  n=add(Num:new(), {1,2,3,4,5,6,7,8})
-  print(n:var()) end
+Eg["num"]=  function()
+   print(add(Num:new(),{1,2,3,4,5,6,7,8}):var())
+end
 
 -- ### Main
-if debug.getinfo(2) then Eg[defaults().todo]() end
+
+-- if sub-module, comment out this line
+Eg[config().todo]() 
 for k,_ in pairs(_ENV) do if not b4[k] then print("?? "..k) end end
+return  {Num=Num,Sym=Sym,Sample=Sample}
---
+++ src/reason.lua
@@ -0,0 +1,93 @@
+local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
+local wght, klass, num, map, just, cli, csv, main
+local Seed, srand, rand, randi
+
+function weight(s) s=s or ""; return s=="-" and -1 or (s=="+" and 1 or 0) end
+function klassp(s) return s:find"+" or s:find"-" or s:find"!" end
+function nump(s)   return s:sub(1,1):match"[A-Z]" end
+function skipp(s)  return s:find"?" end
+ 
+function map(t,f,     b)
+  b={}; for i,v in pairs(t) do b[i]=f(v) end 
+  return b end 
+
+function with(t,f,   b)
+  b={}; for i,v in pairs(t) do if f(v) then b[i]=f(v) end end
+  return b end 
+
+function without(t,f) return with(t,function(v) return not f(v) end) end
+
+function cli(t)
+  for flag,b4 in pairs(t) do
+    for n,x in pairs(arg) do if x==("-"..flag) then return
+      type(b4)=="boolean" and true or tonumber(arg[n+1]) or arg[n+1] end end
+    return b4 end end
+
+function str(t)
+  local function kv(a)
+    for k,v in pairs(t) do a[1+#a]=fmt("%s=%s", k, str(v)) end 
+    return a end
+  return type(t)~="table" and tostring(t) or  ( 
+    #t>0 and '{'..table.concat(map(t,tostring),", ")..'}' or str(kv({}))) end
+
+Seed = 10014
+function srand(s) Seed= s or 10013 end
+function randi(lo,hi) return math.floor(0.5 + rand(lo,hi)) end
+function rand(lo,hi)
+   lo, hi = lo or 0, hi or 1
+   Seed = (16807 * Seed) % 2147483647 
+   return lo + (hi-lo) * Seed / 2147483647 end 
+
+function csv(file,      split,stream,tmp)
+  stream = file and io.input(file) or io.input()
+  tmp    = io.read()
+  return function(       t)
+    if tmp then
+      t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
+      for y in string.gmatch(tmp, "([^,]+)") do t[#t+1]=y end
+      tmp = io.read()
+      if  #t > 0 
+      then for j,x in pairs(t) do t[j] = tonumber(x) or x end
+           return t end
+    else io.close(stream) end end end
+
+function sample()   return {head={},rows={},cols={},num={},sym={},x={},y={}} end
+function num(c,s)   return col(c,s, {hi=-1E64, lo=1E63, all={}}) end
+function sym(c,s)   return col(c,s, {has={}}) end
+function col(c,s,i) i= i or {}; i.n,i.at,i.txt,i.w = 0,c,s or "",weight(s) 
+                    return i end
+
+function add(s,t) 
+  local function add1(col,x)
+    if x ~= "?" then 
+      col.n = col.n+1
+      if   col.lo 
+      then col.all[ 1+#col.all ] = x
+           col.lo = math.min(col.lo, x)
+           col.hi = math.max(col.hi, x)
+      else col.has = 1+(col.has[x] or 0) end end 
+  end ------------------------------------------
+  if #s.head == 0 then
+    s.head = t
+    for c,s in pairs(t) do 
+      new = (skipp(s) and col or (nump(s) and num or sym))(c,s)
+      s.cols[ 1+#s.cols ] = new
+      if not skipp(s) then
+        here= nump(s)  and s.num or s.sym; here[ 1+#here ] = new
+        here= goalp(s) and s.y   or s.x  ; here[ 1+#here ] = new
+  else    
+    s.rows[ 1+#s.rows ] = t 
+    for c,col in pairs(s.cols) do add1(col, t[c]) end end end end end
+
+function main(the, s)
+  srand(the.seed)
+  s = sample()
+  for t in csv(the.data) do add(s,t) end end
+
+main{data = cli{d="../data/auto93.csv"},
+     far  = cli{f=.9}, 
+     p    = cli{p=2}, 
+     some = cli{s=256}, 
+     seed = cli{S=10014}}
+
+for k,_ in pairs(_ENV) do if not b4[k] then print("?? "..k) end end
