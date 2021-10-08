8663a4adf2880022e619a8ebd55a7d4bbbfd686f
Tim Menzies <timm@users.noreply.github.com>
2021-10-08 14:40:48 -0400

    save

--- src/about.lua
+++ src/about.lua
@@ -1,16 +1,17 @@
-local function cli(flag, b4)
-  for n,v in pairs(arg) do if v==flag then 
-    return (b4==false) and true or (tonumber(arg[n+1]) or arg[n+1]) end end 
-  return b4 end
-
-return {
-  combine= cli("-c", "mode"),
-  data=    cli("-d", "../data/auto93.csv"),
-  k=       cli("-k", 2),  
-  p=       cli("-p", 2), 
-  seed=    cli("-S", 10014),   -- random number see
-  some=    cli("-s", 256),     -- use this many randomly nearest neighbors
-  todo=    cli("-t", "hello"), -- default start up is to run Eg["todo"]
-  wait=    cli("-w", 10)       -- start classifying after this many rows
- }
-
+-- vim:filetype=lua :
+	local function cli(flag, b4)
+	  for n,v in pairs(arg) do if v==flag then 
+	    return (b4==false) and true or (tonumber(arg[n+1]) or arg[n+1]) end end 
+	  return b4 end
+	
+	return {
+	  combine= cli("-c", "mode"),
+	  data=    cli("-d", "../data/auto93.csv"),
+	  k=       cli("-k", 2),  
+	  p=       cli("-p", 2), 
+	  seed=    cli("-S", 10014),   -- random number see
+	  some=    cli("-s", 256),     -- use this many randomly nearest neighbors
+	  todo=    cli("-t", "hello"), -- default start up is to run Eg["todo"]
+	  wait=    cli("-w", 10)       -- start classifying after this many rows
+	 }
+	
